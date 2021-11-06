9ed47a6d5df29db129ffcf800ab448306914f43d
marshackVB <marshackVB>
2021-11-06 01:19:58 +0000

    refactored for batch inference

--- model_inference.py
+++ model_inference.py
@@ -10,11 +10,15 @@
 dbutils.widgets.text("model_name",  '')
 dbutils.widgets.text("stage",       '')
 
-# Expects database.table_name
-dbutils.widgets.text("input_df",    '')
-dbutils.widgets.text("output_df",   '')
+dbutils.widgets.text("input_database",  'default')
+dbutils.widgets.text("input_table",     'banking77_train')
+
+dbutils.widgets.text("output_database", 'default')
+dbutils.widgets.text("output_table",     '')
 
 dbutils.widgets.text("feature_col", '')
+dbutils.widgets.text("batch_size", '1000')
+
 model_name =      dbutils.widgets.get("model_name")
 stage =           dbutils.widgets.get("stage")
 
@@ -31,49 +35,113 @@ client = MlflowClient()
 
 # Copy requirements.txt from MLflow to driver
 client.download_artifacts(run_id, 
-                          "model/requirements.txt", 
-                          "/usr/")
+                          "mlflow_model/requirements.txt", 
+                          "/usr")
 
-%pip install -q -r /usr/model/requirements.txt
+%pip install -q -r /usr/mlflow_model/requirements.txt
 
 # COMMAND ----------
 
+import torch
+import torch.nn as nn
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from pyspark.sql.types import ArrayType, FloatType
-from helpers import get_run_id
+import pyspark.sql.functions as func
+from pyspark.sql.functions import col
+from helpers import get_run_id, get_parquet_files, get_config
 import mlflow
+from mlflow.tracking import MlflowClient
 
 # COMMAND ----------
 
-model_name =      dbutils.widgets.get("model_name")
-stage =           dbutils.widgets.get("stage")
-input_df =        dbutils.widgets.get("input_df")
-feature_col =     dbutils.widgets.get("feature_col")
-output_df =       dbutils.widgets.get("output_df")
+model_name =         dbutils.widgets.get("model_name")
+stage =              dbutils.widgets.get("stage")
+
+input_database =     dbutils.widgets.get("input_database")
+input_table =        dbutils.widgets.get("input_table")
+
+output_database =    dbutils.widgets.get("output_database")
+output_table =       dbutils.widgets.get("output_table")
+
+feature_col =        dbutils.widgets.get("feature_col")
+batch_size =         int(dbutils.widgets.get("batch_size"))
 
 # COMMAND ----------
 
-# MAGIC %md ### Load production model from registry and apply to new data
+# MAGIC %md Load production model from registry and apply to new data
 
 # COMMAND ----------
 
-run_id = get_run_id(model_name, stage=stage)
+run_id = get_run_id(model_name, stage)
 
-registered_model = f'runs:/{run_id}/model'
+client = MlflowClient()
 
-loaded_model = mlflow.pyfunc.spark_udf(spark, model_uri=registered_model, result_type=ArrayType(FloatType()))
+# Copy artifacts to driver
+client.download_artifacts(run_id=run_id, path="config", dst_path='/')
+client.download_artifacts(run_id=run_id, path="huggingface_tokenizer", dst_path='/')
+client.download_artifacts(run_id=run_id, path="huggingface_model", dst_path='/')
 
-print(f"This model was fit using the following runtime: {loaded_model.metadata.databricks_runtime}")
+# Load models
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-df = spark.table(input_df)
-
-predictions = (df.withColumn('probabilities', loaded_model(feature_col))
-                 .selectExpr(['*', 'array_max(probabilities) as probability'])
-                 .selectExpr(['*', 'array_position(probabilities, probability) - 1 as category']))
+config = get_config('/config/config.yaml')
+tokenizer = AutoTokenizer.from_pretrained('/huggingface_tokenizer')
+model = AutoModelForSequenceClassification.from_pretrained('/huggingface_model').to(device)
 
 # COMMAND ----------
 
-if output_df:
-  predictions.write.mode('overwrite').format('delta').saveAsTable(output_df)
+# MAGIC %md Create data loader
 
-else:
-  display(predictions)
+# COMMAND ----------
+
+input_files = get_parquet_files(input_database, input_table)
+load_train = load_dataset("parquet", data_files=input_files)
+
+# COMMAND ----------
+
+# MAGIC %md Forward pass for inference
+
+# COMMAND ----------
+
+def forward_pass(batch, tokenizer=tokenizer, model=model, max_length=config.max_token_length, feature_col=feature_col):
+  
+  tokenized = tokenizer(batch[feature_col], 
+                        padding='max_length', 
+                        truncation=True, 
+                        max_length=max_length)
+  
+  input_ids = torch.tensor(tokenized['input_ids']).to(device)
+  attention_mask = torch.tensor(tokenized['attention_mask']).to(device)
+  
+  model = nn.DataParallel(model.cuda())
+  
+  with torch.no_grad():
+    predictions = model(input_ids, attention_mask).logits
+
+  softmax = torch.nn.Softmax(dim=1)
+  predictions = softmax(predictions).detach().cpu().numpy()
+   
+  batch['probabilities'] = predictions
+  
+  return batch
+
+# COMMAND ----------
+
+# MAGIC %md Batch in records, apply model, and write final predictions to a Delta table
+
+# COMMAND ----------
+
+predictions = load_train.map(forward_pass, batched=True, batch_size=batch_size)
+
+predictions.set_format(type='pandas')
+predictions = predictions['train'][:]
+
+predictions_df = (spark.createDataFrame(predictions)
+                       .selectExpr(['*', 'array_max(probabilities) as predicted_probability'])
+                       .selectExpr(['*', 'array_position(probabilities, predicted_probability) - 1 as predicted_class']))
+
+# Can change to 'append' to add to a table of predictions
+predictions_df.write.mode('overwrite').format('delta').saveAsTable(f'{output_database}.{output_table}')
+
+display(predictions_df)
